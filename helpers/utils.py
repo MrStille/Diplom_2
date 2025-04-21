d1a07ac0ec641d59ade453dea6b5350dd3128aa3
Konstantin Mikhalev <k.mikhalev@vk.team>
2025-04-21 19:55:22 +0300

    add allure results

--- helpers/utils.py
+++ helpers/utils.py
@@ -9,14 +9,6 @@ class ApiHelper:
         return f"test-{ApiHelper.generate_random_string(4)}-@yandex.ru"
 
     @staticmethod
-    def random_courier():
-        return {
-            "login": ApiHelper.generate_random_string(10),
-            "password": "Password123AP",
-            "firstName": ApiHelper.generate_random_string(10)
-        }
-
-    @staticmethod
     def generate_random_string(length):
         letters = string.ascii_lowercase
         random_string = ''.join(random.choice(letters) for i in range(length))
